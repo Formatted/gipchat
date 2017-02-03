8a5f84a5ebacad2f8bcf2c2f682a7c3b4da0fa98
formatted <ronivegh@gmail.com>
2017-02-03 14:00:55 -0700

    have a side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,79 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MaterialTitlePanel from './MaterialTitlePanel';
+import SidebarContent from './SidebarContent';
+import Sidebar from './Sidebar';
 import InputBox from './InputBox';
 
-class App extends Component {
+const styles = {
+  contentHeaderMenuLink: {
+    textDecoration: 'none',
+    color: 'white',
+    padding: 8,
+  },
+  content: {
+    padding: '16px',
+  },
+};
+
+const App = React.createClass({
+  getInitialState() {
+    return {docked: false, open: false};
+  },
+
+  componentWillMount() {
+    const mql = window.matchMedia(`(min-width: 800px)`);
+    mql.addListener(this.mediaQueryChanged);
+    this.setState({mql: mql, docked: mql.matches});
+  },
+
+  componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  },
+
+  onSetOpen(open) {
+    this.setState({open: open});
+  },
+
+  mediaQueryChanged() {
+    this.setState({docked: this.state.mql.matches});
+  },
+
+  toggleOpen(ev) {
+    this.setState({open: !this.state.open});
+
+    if (ev) {
+      ev.preventDefault();
+    }
+  },
 
   render() {
+    const sidebar = <SidebarContent />;
+
+    const contentHeader = (
+      <span>
+        {!this.state.docked &&
+         <a onClick={this.toggleOpen} href="#" style={styles.contentHeaderMenuLink}>=</a>}
+        <span> GipChat w/ AI</span>
+      </span>);
+
+    const sidebarProps = {
+      sidebar: sidebar,
+      docked: this.state.docked,
+      open: this.state.open,
+      onSetOpen: this.onSetOpen,
+    };
+
     return (
-      <div className="App">
-        <p>GipChat w/ AI</p>
-        <InputBox />
-      </div>
+      <Sidebar {...sidebarProps}>
+        <MaterialTitlePanel title={contentHeader}>
+          <div style={styles.content}>
+            <InputBox />
+          </div>
+        </MaterialTitlePanel>
+      </Sidebar>
     );
-  }
-}
+  },
+});
 
 export default App;
